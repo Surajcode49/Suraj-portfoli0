c8388378a15cf3bad00b611db2955ecb4de7905d
Suraj kumar <devilsuraj9641@gmail.com>
2024-03-25 03:20:48 +0530

    2nd

---
+++ components/Avatar.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none">
+      <Image
+        src="/sk1.png"
+        alt="avatar"
+        width={400}
+        height={300}
+        className="translate-z-0 w-full h-full"
+      />
+    </div>
+  );
+};
+
+export default Avatar;
